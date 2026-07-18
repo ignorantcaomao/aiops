5e2b7e4480bf328aeb3b9c1a3ef45c58c12c3832
caomaodjs <caomaodjs@caomaodjsdeMac-mini.local>
2026-07-18 11:32:35 +0800

    add webhook

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def main():
-    print("Hello from aiops!")
+from fastapi import FastAPI
 
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
